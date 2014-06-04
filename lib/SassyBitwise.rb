6dbe4ceec07290d8a00ca34059237113401d65fe
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-04 23:32:37 +0200

    Updating lib file

--- lib/SassyBitwise.rb
+++ lib/SassyBitwise.rb
@@ -5,7 +5,7 @@ Compass::Frameworks.register('SassyBitwise', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyBitwise
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
   DATE = "2014-06-04"
 end
 
