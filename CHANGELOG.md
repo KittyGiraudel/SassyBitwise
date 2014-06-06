702d1e7ea902c07c38d5971a4138244b496bb0c9
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-06 21:02:32 +0200

    Bump 1.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # Changelog
 
+* `1.1.1`: reducing Sass and Compass dependencies
 * `1.1.0`: new API accepting multiple bitwise operation simultaneously
 * `1.0.1`: fixing important issue in `^`, `|` and `&`
 * `1.0.0`: stable API, gem release
--- lib/SassyBitwise.rb
+++ lib/SassyBitwise.rb
@@ -5,8 +5,8 @@ Compass::Frameworks.register('SassyBitwise', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyBitwise
-  VERSION = "1.1.0"
-  DATE = "2014-06-05"
+  VERSION = "1.1.1"
+  DATE = "2014-06-06"
 end
 
 module Sass::Script::Functions
