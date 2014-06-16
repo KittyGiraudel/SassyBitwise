3561fa2a86b9ab8f50862a5fff2537b0f6ebc4a1
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-16 08:36:54 +0200

    Bump 1.1.2

--- lib/SassyBitwise.rb
+++ lib/SassyBitwise.rb
@@ -15,6 +15,6 @@ end
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyBitwise
-  VERSION = "1.1.1"
-  DATE = "2014-06-06"
+  VERSION = "1.1.2"
+  DATE = "2014-06-16"
 end
